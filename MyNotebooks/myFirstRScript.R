18fd5e55467c829b28cddeb4ca781045adfc6922
Prahlad G Menon, PhD <menon.prahlad@gmail.com>
2020-07-03 17:16:15 -0400

    Update myFirstRScript.R

--- MyNotebooks/myFirstRScript.R
+++ MyNotebooks/myFirstRScript.R
@@ -1,3 +1,11 @@
- a <- 1
- 
- print(a)
+# Read Chicago COVID19 time series data into R as a data frame
+myData <- read.csv("https://raw.githubusercontent.com/menonpg/covid19_Chicago/master/timeSeries_COVID19_Chicago.csv")
+
+# Convert the date column appropriately so R recognizes it as a Date
+myData$Date <- as.Date(myData$Date, format="%Y-%m-%d")
+
+# Summarize the columns
+print(summary(myData)) 
+
+# Write the file out to the local file system:
+write.csv(myData, "timeSeries_COVID19_Chicago.csv")
