8040e189a3dbda8b1c94271ea5591f14a8c80bd3
Prahlad G Menon, PhD <menon.prahlad@gmail.com>
2020-07-10 16:13:48 -0400

    Update RegressionFromCSV.R

--- MyNotebooks/RegressionFromCSV.R
+++ MyNotebooks/RegressionFromCSV.R
@@ -1,5 +1,5 @@
 library(readr)
-heightWeightSurveyData <- read_csv("heightWeightSurveyData.csv")
+heightWeightSurveyData <- read_csv("heightWeightGenderSurveyData.csv")
 
 heightWeightSurveyData <- na.omit(heightWeightSurveyData)
 View(heightWeightSurveyData)
@@ -10,5 +10,8 @@ summary(fit)
 
 plot(heightWeightSurveyData$Height, heightWeightSurveyData$Weight, xlab="Height", ylab="Weight", main="Regression of Weight on Height")
 abline(fit, col="red")
+
+boxplot( Weight ~ Gender, main="Weight by Gender", data=heightWeightSurveyData)
+boxplot( Height ~ Gender, main="Height by Gender", data=heightWeightSurveyData)
 
 
