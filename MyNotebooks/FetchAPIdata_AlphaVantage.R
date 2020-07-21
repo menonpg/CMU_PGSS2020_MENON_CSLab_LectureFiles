4ec19999e5cf7cf0ab776865b8e65cae433999f7
Prahlad G Menon, PhD <menon.prahlad@gmail.com>
2020-07-21 15:28:31 -0400

    Update FetchAPIdata_AlphaVantage.R

--- MyNotebooks/FetchAPIdata_AlphaVantage.R
+++ MyNotebooks/FetchAPIdata_AlphaVantage.R
@@ -5,6 +5,7 @@ library(TTR)
 
 setAPIKey("YOUR_API_KEY")
 
+# Lets download one symbol's time series data
 sym = "MSFT"
 myData <- fetchSeries(function_nm = "time_series_daily", 
                       symbol = sym, outputsize = "compact", datatype = "json")
@@ -12,3 +13,31 @@ myData <- fetchSeries(function_nm = "time_series_daily",
 myData_XTS <- myData$xts_object
 
 plot(myData_XTS[,-c(5)])  # eliminate the volume column from the XTS plot
+
+
+
+# Lets download a series of time series data for a list of stock tickers
+syms = c("MSFT", "AMZN", "TSLA", "X")
+
+finalDF <- c()
+
+for (sym in syms) {
+  myData <- fetchSeries(function_nm = "time_series_daily", 
+                        symbol = sym, outputsize = "compact", datatype = "json")
+  
+  myData_XTS <- myData$xts_object
+  
+  plot(myData_XTS[,-c(5)], main=sym)
+  
+  
+  colnames(myData_XTS) <- c("Open", "High", "Low", "Close", "Volume")
+  
+  finalDF <- rbind(finalDF, data.frame(TICKER=sym, DATE = rownames(as.data.frame(tail(myData_XTS,1))),
+                                       as.data.frame(tail(myData_XTS,1))))
+  
+  # print(finalDF)
+
+}
+
+rownames(finalDF) <- seq(1, NROW(finalDF), 1)
+print(finalDF)
