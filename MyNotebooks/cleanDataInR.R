bb2e93d27c834a441a4dde085e09c9851f1a68d4
Prahlad G Menon, PhD <menon.prahlad@gmail.com>
2020-07-24 16:12:21 -0400

    Update cleanDataInR.R

--- MyNotebooks/cleanDataInR.R
+++ MyNotebooks/cleanDataInR.R
@@ -19,3 +19,7 @@ boxplot(DataFrameOfCarData_toProcessinR$MSRP[which(DataFrameOfCarData_toProcessi
 plot(density(na.omit(DataFrameOfCarData_toProcessinR$MSRP[which(DataFrameOfCarData_toProcessinR$Drivetrain %in% "AWD")])))
 lines(density(na.omit(DataFrameOfCarData_toProcessinR$MSRP[which(DataFrameOfCarData_toProcessinR$Drivetrain %in% "FWD")])), col= "red")
 
+
+# Statistical testing of difference of means for two categories of DriveTrain, in term sof MSRP, using an unpaired two-samples t-test
+t.test(x = na.omit(DataFrameOfCarData_toProcessinR$MSRP[which(DataFrameOfCarData_toProcessinR$Drivetrain %in% "AWD")]), y = na.omit(DataFrameOfCarData_toProcessinR$MSRP[which(DataFrameOfCarData_toProcessinR$Drivetrain %in% "FWD")]), paired = F, var.equal =  F)
+
